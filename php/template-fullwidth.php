c6d6d6289da57e7c99fc301b104de3e85261fa39
Lars Boesenach <lars@lokaalgevonden.nl>
2014-04-16 16:42:48 +0200

    content bg-white, nav-hovers

--- php/template-fullwidth.php
+++ php/template-fullwidth.php
@@ -8,11 +8,13 @@ Template Name: Page w/o sidebar
 
 	<div>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<article class="u-gridContainer" id="post-<?php the_ID(); ?>">
-			<h2><?php the_title(); ?></h2>
-			<div>
-				<?php the_content(); ?>
-				<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+		<article class="Content u-gridContainer" id="post-<?php the_ID(); ?>">
+			<div class="Content-entry">
+				<h2><?php the_title(); ?></h2>
+				<div class="Content-text">
+					<?php the_content(); ?>
+					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+				</div>
 			</div>
 		</article>
 	<?php endwhile; endif; ?>
